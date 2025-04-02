980aee02baa8b0c170c04de6d1c42298b3763394
santosbarral <sbarral@udesa.edu.ar>
2025-04-02 20:38:32 -0300

    Utilizando los primeros datos de la Api, seccion de pelis Populares en home

--- proyecto-integrador-3/src/App.js
+++ proyecto-integrador-3/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import Favoritos from "./screens/Favoritos/Favoritos";
+<<<<<<< HEAD
 import Home from "./screens/Home /Home";
+=======
+import Home from "./screens/Home/Home"
+import VerTodas from "./screens/VerTodas/VerTodas";
+>>>>>>> de11942 (Utilizando los primeros datos de la Api, seccion de pelis Populares en home)
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import notFound from "./screens/NotFound/NotFound";
@@ -9,6 +14,7 @@ import notFound from "./screens/NotFound/NotFound";
 function App() {
   return (
     <>
+<<<<<<< HEAD
     <main>
     <Header />
       <Switch>
@@ -17,6 +23,17 @@ function App() {
         <Route path='' component={notFound} />
       </Switch>
     </main>
+=======
+      <Header />
+      <main>
+        <Switch>
+          <Route path={'/'} exact={true} component={Home} />
+          <Route path={'/favoritos'} component={Favoritos} />
+          <Route path={'/vertodas'} component={VerTodas} />
+          <Route path='' component={notFound} />
+        </Switch>
+      </main>
+>>>>>>> de11942 (Utilizando los primeros datos de la Api, seccion de pelis Populares en home)
       <Footer />
     </>
 
---
+++ proyecto-integrador-3/src/screens/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "../../components/Home/Home";
+import './style.css'
+
+
+function home (){
+    return (
+        <React.Fragment>
+            <Home/>
+        </React.Fragment> 
+      );
+}
+export default Home;
