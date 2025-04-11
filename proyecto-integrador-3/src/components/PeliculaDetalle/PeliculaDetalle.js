9d1feddfd002770b3a2e65073310c2ff4384ae24
agustinajorba <ajorba@udesa.edu.ar>
2025-04-11 15:25:11 -0300

    Detalle terminado, faltan los estilos y que funcione correctamente Favoritos

--- proyecto-integrador-3/src/components/PeliculaDetalle/PeliculaDetalle.js
+++ proyecto-integrador-3/src/components/PeliculaDetalle/PeliculaDetalle.js
@@ -6,7 +6,8 @@ export default class PeliculaDetalle extends Component {
     console.log(props)
     super(props);
     this.state = {
-      pelicula: {}
+      pelicula: {},
+      favorito: false
     }
   } 
   componentDidMount() {
@@ -17,6 +18,46 @@ export default class PeliculaDetalle extends Component {
     .then((data) => {console.log(data);
      this.setState({ pelicula: data });})
     .catch((error) => {console.log('El error fue: ' + error);})
+
+  }
+
+  chequearFavoritos (){
+    let favoritos = localStorage.getItem('fav')
+    if (favoritos !== null) {
+      let favsParseados = JSON.parse(favoritos);
+      let estaMiId = favsParseados.includes(this.state.pelicula.id);
+      if (estaMiId) {
+        this.setState({ favorito: true });
+      }
+    }
+  }
+
+  agregarFavoritos (id){
+    let favoritos = localStorage.getItem('fav')
+    if (favoritos !== null) {
+      let arrParseado = JSON.parse(favoritos)
+      arrParseado.push(id);
+      localStorage.setItem('fav', JSON.stringify(arrParseado));
+    } else{
+      let primerID = [id]
+      let arrStringificado = JSON.stringify(primerID)
+        localStorage.setItem('fav', arrStringificado)
+    }
+
+    this.setState({
+      favorito: true
+    })
+  }
+
+  sacarFavoritos(id){
+    let favoritos = localStorage.getItem('fav')
+    let favsParseados = JSON.parse(favoritos);
+    let filtrarFavs = favsParseados.filter((elm) => elm !== id )
+    const favsStringificado = JSON.stringify(filtrarFavs)
+      localStorage.setItem('fav', favsStringificado)
+      this.setState({
+        favorito: false
+      })
   }
 
   render() {
@@ -24,7 +65,7 @@ export default class PeliculaDetalle extends Component {
     
     return (
      
-      <div>
+      <div className='errorDetail'>
         {!movies.title ? (
           <p>No se encontró ninguna película con ese ID.</p>
         ) : (
@@ -36,6 +77,12 @@ export default class PeliculaDetalle extends Component {
             <p>Duración: {movies.runtime} minutos</p>
             <p>Sinopsis: {movies.overview}</p>
             <p>Género/s: {movies.genres.map(genero => genero.name).join(', ')}</p>
+            {
+          this.state.favorito ?
+          <button onClick={()=> this.sacarFavoritos(movies.id) }>Sacar del carrito</button>
+          :
+          <button onClick={() => this.agregarFavoritos(movies.id)}>Agregar al carrito</button>
+        }
             </div>
             <img src={`https://image.tmdb.org/t/p/w500/${movies.poster_path}`} alt={movies.title} className='foto-detail'/>
           </section>
