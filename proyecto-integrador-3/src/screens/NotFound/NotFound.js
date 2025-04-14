9b0e0f4650ada84c61db0cc939f5a2356d290092
martinaFCovaro <mfernandezcovaro@gmail.com>
2025-04-14 10:53:32 -0300

    termine lo de sacar de favoritos

--- proyecto-integrador-3/src/screens/NotFound/NotFound.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './style.css'
-
-export default function NotFound() {
-  return (
-    <div className='texto-notFound main-notFound'>Error 404 -- Not Found</div>
-  )
-}
